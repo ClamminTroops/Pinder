5c801c33ab8d32b87e3c41aa59382e0fe59d14d5
carolinecarlson <cjc39@students.calvin.edu>
2018-10-12 14:47:31 -0400

    Update 3 for result activity with the added layout and java files

--- app/src/main/java/edu/calvin/cs262/teamc/MatchActivity.java
+++ app/src/main/java/edu/calvin/cs262/teamc/MatchActivity.java
@@ -2,6 +2,7 @@ package edu.calvin.cs262.teamc;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MatchActivity extends AppCompatActivity {
 
@@ -9,5 +10,8 @@ public class MatchActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_match);
+    }
+
+    public void onAdoptOneBtnPressed(View view) {
     }
 }
