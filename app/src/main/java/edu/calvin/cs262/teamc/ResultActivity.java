a804087833167d5c2baf5a72d1ff2b40a3830e76
carolinecarlson <cjc39@students.calvin.edu>
2018-10-12 14:47:31 -0400

    Update 3 for result activity with the added layout and java files

--- app/src/main/java/edu/calvin/cs262/teamc/ResultActivity.java
+++ app/src/main/java/edu/calvin/cs262/teamc/ResultActivity.java
@@ -2,6 +2,7 @@ package edu.calvin.cs262.teamc;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class ResultActivity extends AppCompatActivity {
 
@@ -9,5 +10,8 @@ public class ResultActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
+    }
+
+    public void onAdoptOneBtnPressed(View view) {
     }
 }
