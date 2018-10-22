202502d6e5cea95878511a0859a6173bf09d3d73
Andrew Thomas <andrewdt97@gmail.com>
2018-10-22 18:03:12 -0400

    Added result page with refactor

---
+++ app/src/main/java/edu/calvin/cs262/teamc/ResultActivity.java
@@ -0,0 +1,18 @@
+package edu.calvin.cs262.teamc;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class ResultActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_result);
+    }
+
+    public void onAdoptOneBtnPressed(View view) {
+    }
+
+}
